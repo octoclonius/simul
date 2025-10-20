8c657f723b32d826f9f3217d6c455950b26e4716
octoclonius <25781800+octoclonius@users.noreply.github.com>
2025-10-19 19:43:18 -0500

    nix-develop-gha

--- flake.nix
+++ flake.nix
@@ -1,16 +1,23 @@
 {
   inputs = {
+    nix-develop-gha = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nicknovitski/nix-develop";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = {
     self,
+    nix-develop-gha,
     nixpkgs,
+    ...
   }:
   let
     pkgs = nixpkgs.legacyPackages.aarch64-linux;
   in
   {
+    packages.aarch64-linux.nix-develop-gha = nix-develop-gha.packages.aarch64-linux.default;
     devShells.aarch64-linux.default = pkgs.mkShell {
       packages = with pkgs.python312Packages; [
         pettingzoo
